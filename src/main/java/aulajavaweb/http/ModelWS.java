d1b70f21289db5c3877b370ad2710461829fa71e
renantdesouza <java02@trn52.(none)>
2015-10-23 19:28:18 -0200

    configs

--- src/main/java/aulajavaweb/http/ModelWS.java
+++ src/main/java/aulajavaweb/http/ModelWS.java
@@ -3,7 +3,9 @@ package aulajavaweb.http;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -15,9 +17,17 @@ public class ModelWS {
 
 	@GET
 	@Path("all")
-	@Produces({MediaType.APPLICATION_JSON})
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Produto> model() {
 		return Arrays.asList(Produto.builder().id(1).name("coca-cola").build());
 	}
+	
+	@POST
+	@Path("insert")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void insert(Produto produto) {
+		
+	} 
 	
 }
