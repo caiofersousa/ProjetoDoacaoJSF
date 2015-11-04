3100183b683c8c33d6996f999d5a1b3bd172f08d
caiofersousa <caiofersousa@hotmail.com>
2015-11-04 20:15:18 -0200

    alteração no enum de gatos

--- src/main/java/aulajavaweb/service/AnimalService.java
+++ src/main/java/aulajavaweb/service/AnimalService.java
@@ -1,7 +1,17 @@
 package aulajavaweb.service;
 
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
 
 public class AnimalService extends Service {
+
+	protected AnimalService(URL wsdlDocumentLocation, QName serviceName) {
+		super(wsdlDocumentLocation, serviceName);
+		// TODO Auto-generated constructor stub
+	}
 //	private AnimalDao dao  = new AnimalDao();
 //	private AnimalValidador validador = new AnimalValidador();
 
